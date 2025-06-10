ccd1bcae9224e307b64fb019d009f1add32eb103
Streaming-code <140299641+Streaming-code@users.noreply.github.com>
2025-06-10 15:46:59 +0800

    Add files via upload

---
+++ DRL_model_train/controller_back_up.py
@@ -0,0 +1,189 @@
+# input: download_video_id, bitrate, sleep_time
+# output: info needed by schedule algorithm
+# buffer: ms
+
+import numpy as np
+import math
+from simulator.video_player import Player, VIDEO_CHUNCK_LEN
+from simulator.user_module import Retention
+from simulator.network_module import Network
+
+USER_FILE = 'logs/sample_user/user.txt'
+# user_file = open(USER_FILE, 'wb')
+LOG_FILE = 'logs/log.txt'
+
+NEW = 0
+DEL = 1
+
+VIDEO_BIT_RATE = [750, 1200, 1850]  # Kbps
+PLAYER_NUM = 5
+
+class Environment:
+    def __init__(self, user_sample_id, all_cooked_time, all_cooked_bw, video_num, seeds):
+        self.players = []
+        self.seeds = seeds
+        global USER_FILE
+        USER_FILE = 'logs/sample_user/user_'+str(user_sample_id)+'.txt'
+        self.user_file = open(USER_FILE, 'wb')
+        self.user_models = []  # Record the user action(Retention class) for the current video, update synchronized with players
+        self.video_num = video_num
+        self.video_cnt = 0
+        self.play_video_id = 0
+        self.network = Network(all_cooked_time, all_cooked_bw)
+        self.timeline = 0.0
+        # for ratio
+        self.total_watched_len = 0.0
+        self.total_downloaded_len = 0.0
+
+        # print(self.watch_ratio)
+
+        # self.download_permit = set()
+        for p in range(PLAYER_NUM):
+            # self.download_permit.add(p)
+            self.players.append(Player(p))
+            user_time, user_retent_rate = self.players[-1].get_user_model()
+            self.user_models.append(Retention(user_time, user_retent_rate, seeds[self.video_cnt]))
+            self.total_watched_len += self.user_models[-1].get_ret_duration()  # sum the total watch duration
+            self.video_cnt += 1
+            self.user_file.write((str(self.user_models[-1].get_ret_duration()) + '\n').encode())
+            self.user_file.flush()
+        
+        self.start_video_id = 0
+        self.end_video_id = PLAYER_NUM - 1
+
+    def player_op(self, operation):
+        if operation == NEW:
+            # print('--------------ADD--------------')
+            if self.video_cnt >= self.video_num:  # If exceed video cnt, no add
+                return
+            self.players.append(Player(self.video_cnt))
+            self.end_video_id += 1
+            # print("adding: ", self.video_num)
+            user_time, user_retent_rate = self.players[-1].get_user_model()
+            self.user_models.append(Retention(user_time, user_retent_rate, self.seeds[self.video_cnt]))
+            self.video_cnt += 1
+            self.total_watched_len += self.user_models[-1].get_ret_duration()  # sum the total watch duration
+            # record the user retention rate
+            # user_file.write((str(self.players[-1].get_watch_duration()) + '\n').encode())
+            self.user_file.write((str(self.user_models[-1].get_ret_duration()) + '\n').encode())
+            self.user_file.flush()
+        else:
+            # print('--------------DEL--------------')
+            self.players.remove(self.players[0])
+            self.user_models.remove(self.user_models[0])
+    
+    def get_start_video_id(self):
+        return self.start_video_id
+
+    def get_wasted_time_ratio(self):
+        return self.total_downloaded_len / self.total_watched_len
+
+    def play_videos(self, action_time):  # play for action_time from the start of current players queue
+        # print("\n\nPlaying Video ", self.start_video_id)
+        wasted_bw = 0
+        buffer = 0
+
+        # Continues to play if all the following conditions are satisfied:
+        # 1) there's still action_time len
+        # 2) the last video hasn't caused rebuf
+        # 3) the video queue is not empty (will break inside the loop if its already empty)
+        while buffer >= 0 and action_time > 0:
+            # print("time_left:", action_time)
+            # the timeline of the current video before this play step
+            timeline_before_play = self.players[0].play_timeline
+            # print("timeline_before_play: ", timeline_before_play)
+            # the remain time length of the current video
+            video_remain_time = self.user_models[0].get_ret_duration() - timeline_before_play
+            # print("video_remain_time: ", video_remain_time)
+            # the maximum play time of the current video
+            max_play_time = min(action_time, video_remain_time)
+            # print("max_play_time: ", video_remain_time)
+            # timeline_after_play is the actual time when the play action ended( <=max_play_tm + before_play )
+            timeline_after_play, buffer = self.players[0].video_play(max_play_time)
+            # print("timeline_after_play: ", timeline_after_play)
+            # the actual time length of this play action
+            actual_play_time = timeline_after_play - timeline_before_play
+            # print("actual_play_time: ", timeline_after_play)
+            # consume the action_time
+            action_time -= actual_play_time
+
+            # if the current video has ended
+            if actual_play_time == video_remain_time:
+                # Output: the downloaded time length, the total time length, the watch duration
+                print("\nUser stopped watching Video ", self.start_video_id, "( ", self.players[0].get_video_len(), " ms ) :")
+                print("User watched for ", self.user_models[0].get_ret_duration(), " ms, you downloaded ", self.players[0].get_chunk_counter()*VIDEO_CHUNCK_LEN, " sec.")
+
+                # Output the qualities and smooth of this video:
+                smooth = 0
+                video_qualities = []
+                bitrate_cnt = min(math.ceil(self.players[0].get_play_chunk()), self.players[0].get_chunk_sum())
+                # print(math.ceil(self.players[0].get_play_chunk()), "------")
+                for i in range(1, bitrate_cnt):
+                    video_qualities.append(self.players[0].get_video_quality(i-1))
+                    smooth += abs(VIDEO_BIT_RATE[self.players[0].get_video_quality(i)] - VIDEO_BIT_RATE[self.players[0].get_video_quality(i-1)])
+                video_qualities.append(self.players[0].get_video_quality(bitrate_cnt-1))
+                print("Your downloaded bitrates are: ", video_qualities, ", therefore your smooth penalty is: ", smooth)
+
+                # use watch duration as an arg for the calculation of wasted_bandwidth of this current video
+                wasted_bw += self.players[0].bandwidth_waste(self.user_models[0])
+
+                # Forward the queue head to the next video
+                self.player_op(DEL)
+                self.start_video_id += 1
+                self.player_op(NEW)
+                self.play_video_id += 1
+
+            if self.play_video_id >= self.video_num:
+                # if it has come to the end of the list
+                print("played out!")
+                break
+
+        if buffer < 0:  # action ends because a video stuck(needs rebuffer)
+            buffer = (-1) * action_time  # rebuf time is the remain action time(cause the player will stuck for this time too)
+        return buffer, wasted_bw
+              
+    def buffer_management(self, download_video_id, bitrate, sleep_time):
+        buffer = 0
+        rebuf = 0
+        end_of_video = False
+        delay = 0
+        video_size = 0
+        wasted_bytes = 0
+
+        if sleep_time > 0:
+            sleep_time = int(sleep_time)
+            delay = sleep_time
+            buffer, wasted = self.play_videos(sleep_time)
+            # Return the end flag for the current playing video
+            if self.play_video_id == self.video_num:  # if user leaves
+                end_of_video = True
+            else:
+                end_of_video = (self.players[self.play_video_id-self.start_video_id].get_remain_video_num() == 0)
+        else:
+            video_size = self.players[download_video_id-self.start_video_id].get_video_size(bitrate)
+            # print("the actual download size is:", video_size)
+            self.players[download_video_id - self.start_video_id].record_download_bitrate(bitrate)
+            delay = math.floor(self.network.network_simu(video_size)+0.5)  # ms
+            # print("the actual download delay is:", delay)
+            # print("\n\n")
+            # play_timeline, buffer = self.players[self.play_video_id - self.start_video_id].video_play(delay)
+            buffer, wasted = self.play_videos(delay)
+            self.total_downloaded_len += VIDEO_CHUNCK_LEN  # sum up the total downloaded time
+            if download_video_id < self.start_video_id:
+                # If the video has already been ended, we only accumulate the wastage
+                print("Extra chunk downloaded for Video ", download_video_id,
+                      " which the user already finished watching.\n")
+                wasted += video_size  # Since its already fluently played, the download must be redundant
+                end_of_video = True
+            else:
+                if self.play_video_id == self.video_num:  # if user leaves
+                    end_of_video = True
+                else:
+                    end_of_video = self.players[download_video_id-self.start_video_id].video_download(VIDEO_CHUNCK_LEN)
+
+        # Sum up the bandwidth wastage
+        wasted_bytes += wasted
+        if buffer < 0:
+            rebuf = abs(buffer)
+
+        return delay, rebuf, video_size, end_of_video, self.play_video_id, wasted_bytes
---
+++ DRL_model_train/video_player.py
@@ -0,0 +1,195 @@
+# multi-video play
+import numpy as np
+import math
+import os
+
+# VIDEO_BIT_RATE = [750,1200,1850]  # Kbps
+from config_algorithm import VIDEO_BIT_RATE
+BITRATE_LEVELS = len(VIDEO_BIT_RATE)
+VIDEO_CHUNCK_LEN = 1000.0
+MILLISECONDS_IN_SECOND = 1000.0
+VIDEO_SIZE_FILE = 'data/short_video_size/'
+VIDEO_SIZE_SCALE = 1.0  # chunk size
+USER_RET = './data/user_ret_1s/'
+DISTINCT_VIDEO_NUM = 100
+
+class Player:
+    # initialize each new video and player buffer
+    def __init__(self, video_num, video_size_file, user_ret, chunklength):
+        global VIDEO_SIZE_FILE, USER_RET, VIDEO_CHUNCK_LEN
+        VIDEO_SIZE_FILE = video_size_file
+        USER_RET = user_ret
+        VIDEO_CHUNCK_LEN = chunklength
+        # initialize each new video
+        self.video_size = {}  # in bytes
+        videos = []
+        for root, dirs, files in os.walk(VIDEO_SIZE_FILE):
+            videos = dirs
+            # videos.sort()
+            videos = self.sort_video(videos)
+            break
+
+        # num and len of the video, all chunks are counted instead of -1 chunk
+        idx = 0
+        with open(USER_RET + '../video_names.csv') as f:
+            for line in f:
+                if idx == video_num:
+                    video_info = line.split(',')
+                    self.video_len = float(video_info[2])
+                    self.video_name = video_info[1]
+                idx += 1
+        # print(video_num)
+        # print(self.video_name)
+        # print(self.video_len)
+        # self.video_len = self.chunk_num * VIDEO_CHUNCK_LEN  # ms
+
+        self.chunk_num = int((self.video_len + VIDEO_CHUNCK_LEN - 1) / VIDEO_CHUNCK_LEN)
+        # print(VIDEO_CHUNCK_LEN)
+
+        for bitrate in range(len(VIDEO_BIT_RATE)):
+            self.video_size[bitrate] = []
+            # file_name = VIDEO_SIZE_FILE + self.video_name + '/video_size_' + str(bitrate)
+            # with open(file_name) as f:
+            #     for line in f:
+            #         self.video_size[bitrate].append(int(line.split()[0])/VIDEO_SIZE_SCALE)
+            for i in range(self.chunk_num):
+                self.video_size[bitrate].append(VIDEO_BIT_RATE[bitrate] * VIDEO_CHUNCK_LEN / 1000. * 1000. / 8) # B
+        # print(self.video_size)
+        
+        # download chunk counter
+        self.video_chunk_counter = 0
+        self.video_chunk_remain = self.chunk_num - self.video_chunk_counter
+        
+        self.download_chunk_bitrate = []
+        
+        # play chunk counter
+        self.video_play_counter = 0
+        
+        # play timeline of this video
+        self.play_timeline = 0.0
+        
+        # initialize the buffer
+        self.buffer_size = 0  # ms
+        
+        # initialize preload size
+        self.preload_size = 0 # B
+
+        # initialize corresponding user watch time
+        self.user_time = []  # user_model which players have access to!!
+        self.user_retent_rate = []
+
+        with open(USER_RET + self.video_name) as file:
+            for line in file:
+                self.user_time.append(float(line.split()[0]) * MILLISECONDS_IN_SECOND)
+                self.user_retent_rate.append(line.split()[1])
+
+    def get_user_model(self):
+        return self.user_time, self.user_retent_rate
+    
+    def get_video_len(self):
+        return self.video_len
+
+    def get_video_size(self, quality):
+        try:
+            video_chunk_size = self.video_size[quality][self.video_chunk_counter]
+        except IndexError:
+            raise Exception("You're downloading chunk ["+str(self.video_chunk_counter)+"] is out of range. "\
+                            + "\n   % Hint: The valid chunk id is from 0 to " + str(self.chunk_num-1) + " %")
+        # self.preload_size += video_chunk_size
+        return video_chunk_size
+
+    def get_video_quality(self, chunk_id):
+        if chunk_id >= len(self.download_chunk_bitrate) or chunk_id < 0:
+            return -1  # means no video downloaded
+        return self.download_chunk_bitrate[chunk_id]
+
+    # get size of all preloaded chunks
+    def get_preload_size(self):
+        return self.preload_size
+    
+    def get_undownloaded_video_size(self, P):  # the undownloaded video size
+        chunk_playing = self.get_chunk_counter()
+        future_videosize = []
+        for i in range(BITRATE_LEVELS):
+            size_in_level = []
+            for k in range(P):
+                size_in_level.append(self.video_size[i][int(chunk_playing+k)])
+            future_videosize.append(size_in_level)
+        return future_videosize
+
+    def get_future_video_size(self, P):  # the unplayed video size
+        interval = 1
+        chunk_playing = self.get_play_chunk()
+        if chunk_playing % 1 == 0:  # Check whether it is an integer
+            interval = 0
+
+        future_videosize = []
+        for i in range(BITRATE_LEVELS):
+            size_in_level = []
+            for k in range(P):
+                size_in_level.append(self.video_size[i][int(chunk_playing + interval + k)])
+            future_videosize.append(size_in_level)
+        return future_videosize
+
+    def get_play_chunk(self):
+        return self.play_timeline / VIDEO_CHUNCK_LEN
+    
+    def get_remain_video_num(self):
+        self.video_chunk_remain = self.chunk_num - self.video_chunk_counter
+        return self.video_chunk_remain
+    
+    def get_chunk_sum(self):
+        return self.chunk_num
+
+    def get_chunk_counter(self):
+        return self.video_chunk_counter
+
+    def get_buffer_size(self):
+        return self.buffer_size
+    
+    def record_download_bitrate(self, bit_rate):
+        self.download_chunk_bitrate.append(bit_rate)
+        self.preload_size += self.video_size[bit_rate][self.video_chunk_counter]
+    
+    def get_downloaded_bitrate(self):
+        return self.download_chunk_bitrate
+    
+    def bandwidth_waste(self, user_ret):
+        download_len = len(self.download_chunk_bitrate)
+        waste_start_chunk = math.ceil(user_ret.get_ret_duration() / VIDEO_CHUNCK_LEN)
+        sum_waste_each_video = 0
+        for i in range(waste_start_chunk, download_len):
+            download_bitrate = self.download_chunk_bitrate[i]
+            download_size = self.video_size[download_bitrate][i]
+            sum_waste_each_video += download_size
+        return sum_waste_each_video
+            
+    # download the video, buffer increase.
+    def video_download(self, download_len):  # ms
+        self.buffer_size += download_len
+        self.video_chunk_counter += 1
+        end_of_video = False
+        if self.video_chunk_counter >= self.chunk_num:
+            end_of_video = True
+        return end_of_video
+
+    # play the video, buffer decrease. Return the remaining buffer, negative number means rebuf
+    def video_play(self, play_time):  # ms
+        buffer = self.buffer_size - play_time
+        self.play_timeline += np.minimum(self.buffer_size, play_time)   # rebuffering time is not included in timeline
+        self.buffer_size = np.maximum(self.buffer_size - play_time, 0.0)
+        # print(self.buffer_size, play_time, "play_timeline", self.play_timeline)
+        return self.play_timeline, buffer
+
+    def sort_video(self, video):
+        sorted_video = [''] * len(video)
+        for i in range(len(video)):
+            video_name = video[i]
+            video_index = int(video_name.split('_')[0])
+            sorted_video[video_index] = video[i]
+
+        return sorted_video
+
+
+
+
